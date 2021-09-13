f998cd94a85b997c08bbaecb02815e985f421cef
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-09-13 13:50:48 +0100

    initial commit

---
+++ components/auth/auth-form.js
@@ -0,0 +1,41 @@
+import classes from './auth-form.module.css';
+import {useState, useRef} from 'react'
+
+
+function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button
+            type='button'
+            className={classes.toggle}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default AuthForm;
---
+++ pages/auth.js
@@ -0,0 +1,8 @@
+import AuthForm from '../components/auth/auth-form';
+
+function AuthPage() {
+
+  return <AuthForm />;
+}
+
+export default AuthPage;
